006c8380cd21850232e612abfb39657a7746efb7
Andrew Betts <andrew.betts@ft.com>
2014-01-06 08:18:42 +0000

    Added tests, modified behaviour to meet expectations

--- main.js
+++ main.js
@@ -2,20 +2,26 @@
 var globalPrefix = '/bower_components';
 var moduleVersions = {}, modulePaths = {};
 
+function trim(s) { return s.replace(/^\/+/, '').replace(/\/+$/, ''); }
+
 module.exports = {
 	setGlobalPathPrefix: function(newprefix) {
 		globalPrefix = newprefix;
 	},
 	setModuleVersions: function (map) {
-		for (var i in map) moduleVersions[i] = map[i];
+		for (var i in map) moduleVersions[i] = trim(map[i]);
 	},
 	setModulePaths: function (map) {
-		for (var i in map) modulePaths[i] = map[i];
+		for (var i in map) modulePaths[i] = trim(map[i]);
 	},
 	resolve: function(path, modulename) {
-		var fullpath = path;
-		if (moduleVersions[modulename]) fullpath = moduleVersions[modulename] + '/' + fullpath;
+		var fullpath = trim(path);
+		if (typeof modulePaths[modulename] == 'undefined') modulePaths[modulename] = modulename;
+
+		if (modulePaths[modulename] && moduleVersions[modulename]) fullpath = moduleVersions[modulename] + '/' + fullpath;
 		if (modulePaths[modulename]) fullpath = modulePaths[modulename] + '/' + fullpath;
-		return globalPrefix+fullpath;
+		fullpath = '/' + fullpath;
+		if (globalPrefix) fullpath = globalPrefix + fullpath;
+		return fullpath;
 	}
 };
