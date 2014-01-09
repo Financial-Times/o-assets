a0d91fe7c6ba5a55fd15d081dcc6d65539be8a93
Rhys Evans <rhys.evans@ft.com>
2014-01-09 12:07:48 +0000

    chainability

--- main.js
+++ main.js
@@ -7,12 +7,15 @@ function trim(s) { return s.replace(/^\/+/, '').replace(/\/+$/, ''); }
 module.exports = {
 	setGlobalPathPrefix: function (newprefix) {
 		if (typeof newprefix != 'undefined') globalPrefix = newprefix;
+		return this;
 	},
 	setModuleVersions: function (map) {
 		for (var i in map) moduleVersions[i] = trim(map[i]);
+		return this;
 	},
 	setModulePaths: function (map) {
 		for (var i in map) modulePaths[i] = trim(map[i]);
+		return this;
 	},
 	resolve: function (path, modulename) {
 		var fullpath = trim(path);
