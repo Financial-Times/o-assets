b7696fd36c8313141d4079baf51c1f8e5b70fc25
Andrew Betts <andrew.betts@ft.com>
2014-01-12 23:48:02 +0000

    Add basic tests for JS implementation

---
+++ test/tests.js
@@ -0,0 +1,58 @@
+/*
+
+Rudimentary tests for o-asset path resolver
+
+To run, browserify this JS file and then run it using node
+
+  browserify tests.js -o testrunner.js
+  node testrunner.js
+
+*/
+
+var assets = require('../main.js');
+
+function assertEquals(test, expect, message) {
+    if (test !== expect) {
+    	console.trace();
+        throw message || "Expected '"+expect+"', got '"+test+"'";
+    }
+}
+
+assertEquals(assets.resolve('/img/logo.png', 'module1'), '/bower_components/module1/img/logo.png');
+assertEquals(assets.resolve('img/logo.png', 'module1'), '/bower_components/module1/img/logo.png');
+
+assets.setGlobalPathPrefix('foo');
+assertEquals(assets.resolve('/img/logo.png', 'module1'), 'foo/module1/img/logo.png');
+
+assets.setModulePaths({
+	module1: 'module1modified',
+	module2: '/module2modified/'
+});
+assertEquals(assets.resolve('/img/logo.png', 'module1'), 'foo/module1modified/img/logo.png');
+assertEquals(assets.resolve('/img/logo.png', 'module2'), 'foo/module2modified/img/logo.png');
+assertEquals(assets.resolve('/img/logo.png', 'module3'), 'foo/module3/img/logo.png');
+assertEquals(assets.resolve('img/logo.png', 'module1'), 'foo/module1modified/img/logo.png');
+assertEquals(assets.resolve('img/logo.png', 'module2'), 'foo/module2modified/img/logo.png');
+assertEquals(assets.resolve('img/logo.png', 'module3'), 'foo/module3/img/logo.png');
+
+assets.setModuleVersions({
+	module1: '3.4.2',
+	module3: '1.0.0'
+});
+assertEquals(assets.resolve('/img/logo.png', 'module1'), 'foo/module1modified/3.4.2/img/logo.png');
+assertEquals(assets.resolve('/img/logo.png', 'module2'), 'foo/module2modified/img/logo.png');
+assertEquals(assets.resolve('/img/logo.png', 'module3'), 'foo/module3/1.0.0/img/logo.png');
+
+assets.setGlobalPathPrefix('');
+assertEquals(assets.resolve('/img/logo.png', 'module1'), '/module1modified/3.4.2/img/logo.png');
+assertEquals(assets.resolve('/img/logo.png', 'module2'), '/module2modified/img/logo.png');
+assertEquals(assets.resolve('/img/logo.png', 'module3'), '/module3/1.0.0/img/logo.png');
+
+assets.setModulePaths({
+	module1: '',
+	module2: ''
+});
+assertEquals(assets.resolve('/img/logo.png', 'module1'), '/img/logo.png');
+assertEquals(assets.resolve('/img/logo.png', 'module2'), '/img/logo.png');
+assertEquals(assets.resolve('/img/logo.png', 'module3'), '/module3/1.0.0/img/logo.png');
+
