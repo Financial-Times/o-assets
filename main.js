fa332121a3e6eb7ec8eda0a9c44075cdf2b35205
Kaelig <kaelig@deloumeau.fr>
2014-11-19 12:21:20 +0000

    Fixes to pass linting

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+'use strict';
 
 var globalPrefix = '/bower_components/';
 var moduleVersions = {}, modulePaths = {};
@@ -5,32 +6,40 @@ var moduleVersions = {}, modulePaths = {};
 function trim(s) { return s.replace(/^\/+/, '').replace(/\/+$/, ''); }
 
 module.exports = {
-    setGlobalPathPrefix: function (newprefix) {
-        if (typeof newprefix != 'undefined') globalPrefix = newprefix;
-        return this;
-    },
-    // left in for backwards compatibility but shouldn't be needed hereonin
-    setModuleVersions: function (map) {
-        for (var i in map) moduleVersions[i] = trim(map[i]);
-        return this;
-    },
-    setModulePaths: function (map) {
-        for (var i in map) modulePaths[i] = trim(map[i]);
-        return this;
-    },
-    resolve: function (path, modulename) {
-        var fullpath = trim(path);
-        if (typeof modulePaths[modulename] == 'undefined') modulePaths[modulename] = modulename;
+	setGlobalPathPrefix: function (newprefix) {
+		if (typeof newprefix !== 'undefined') globalPrefix = newprefix;
+		return this;
+	},
+	// left in for backwards compatibility but shouldn't be needed hereonin
+	setModuleVersions: function (map) {
+		for (var i in map) {
+			if (i) {
+				moduleVersions[i] = trim(map[i]);
+			}
+		}
+		return this;
+	},
+	setModulePaths: function (map) {
+		for (var i in map) {
+			if (i) {
+				modulePaths[i] = trim(map[i]);
+			}
+		}
+		return this;
+	},
+	resolve: function (path, modulename) {
+		var fullpath = trim(path);
+		if (typeof modulePaths[modulename] === 'undefined') modulePaths[modulename] = modulename;
 
-        if (modulePaths[modulename]) {
-            fullpath = modulePaths[modulename] +
+		if (modulePaths[modulename]) {
+			fullpath = modulePaths[modulename] +
 
-            // left in for backwards compatibility but shouldn't be needed hereonin
-            (moduleVersions[modulename] ? ('@' +  moduleVersions[modulename]) : '') +
+			// left in for backwards compatibility but shouldn't be needed hereonin
+			(moduleVersions[modulename] ? ('@' +  moduleVersions[modulename]) : '') +
 
-            '/' + fullpath;
-        }
-        if (globalPrefix) fullpath = globalPrefix + fullpath;
-        return fullpath;
-    }
+			'/' + fullpath;
+		}
+		if (globalPrefix) fullpath = globalPrefix + fullpath;
+		return fullpath;
+	}
 };
