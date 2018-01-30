1a6fff29027f31d9225419c23c043d08d33f32e2
Gabrielle von Koss <gabivk@gmail.com>
2018-01-30 16:41:58 +0000

    add to task

--- test-sass/assets.test.js
+++ test-sass/assets.test.js
@@ -5,11 +5,6 @@ const proclaim = require('proclaim');
 const oAssets = require('../main');
 
 describe('o-assets', () => {
-	// it('should pass CSS tests', () => {
-	// 	const css = readFileSync(require('path').join(process.cwd(), 'build/test.css'), 'utf8');
-	// 	proclaim.include(css, 'Failed: 0');
-	// });
-
 	it('#resolve', () => {
 		proclaim.deepEqual(oAssets.resolve('/img/logo.png', 'module1'), '/bower_components/module1/img/logo.png');
 		proclaim.deepEqual(oAssets.resolve('img/logo.png', 'module1'), '/bower_components/module1/img/logo.png');
